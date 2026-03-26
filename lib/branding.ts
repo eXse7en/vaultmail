4585950dbfa1c99f4bc48e99a53a5156e46cab5e
eXse7en <mfkharismawan@gmail.com>
2026-03-26 18:17:44 +0700

    Change default app name from 'YS Mail' to 'Fitroh Mail'

--- lib/branding.ts
+++ lib/branding.ts
@@ -1,4 +1,4 @@
-export const DEFAULT_APP_NAME = 'YS Mail';
+export const DEFAULT_APP_NAME = 'Fitroh Mail';
 
 export const normalizeAppName = (value: unknown) => {
   if (typeof value !== 'string') return '';
